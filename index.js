b4ea8960db6ffe1c4ef1716455aaeba6f2c40254
Rémi Testa <remi.testa@gmail.com>
2018-07-03 15:35:18 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const menubar = require('menubar')
+
+var mb = menubar({
+	index: "https://m.9gag.com",
+	height: '60%',
+	icon: __dirname + '/icon.png',
+});
+
+mb.on('ready', function ready () {
+	console.log('app is ready')
+})
